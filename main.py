39ee9267414521402da24794ebe57eac9937d661
zvi.shiran <zvi.shiran@blich.co.il>
2025-09-07 19:46:53 +0300

    update main file with new logic

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+print("hello my world")
+# first_name = input("enter your first name:")
+# last_name = input("enter your last name:")
+# print(f"{last_name} {first_name}")
+x= 5
+y = 6
+sum = x + y
+print(sum)
+del sum
+print(sum([3, 4]))
